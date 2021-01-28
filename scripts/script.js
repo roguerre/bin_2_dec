52462ce74f22342d900a3fe5199e973a9b676cc8
Ivan Guerrero <ivan.guerrero@dreamcodesoft.com>
2021-01-28 08:15:00 -0500

    notify the user when invalid input has been entered

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const explanation = document.getElementById("explanation");
+const userInput = document.getElementById("user-input");
+const userNotification = document.getElementById("user-notification");
+const userOutput = document.getElementById("user-output");
 const isVariable = document.getElementById("is-variable");
 
 const explanationNormal = `
@@ -11,11 +14,27 @@ const explanationVariable = `
 `;
 
 explanation.innerText = explanationNormal;
+userInput.setAttribute("maxLength", 8);
+
+userInput.addEventListener("input", (evt) => {
+  const pattern = /^[01]+$/;
+  const currValue = evt.target.value;
+  if (currValue.length > 0 && !pattern.test(currValue)) {
+    userNotification.innerText =
+      "Just 1 or 0 are allowed (check for blank spaces also).";
+  } else {
+    userNotification.innerText = "";
+  }
+});
 
 isVariable.addEventListener("click", (evt) => {
+  userInput.value = "";
+  userOutput.value = "";
   if (evt.target.checked) {
     explanation.innerText = explanationVariable;
+    userInput.removeAttribute("maxLength");
   } else {
     explanation.innerText = explanationNormal;
+    userInput.setAttribute("maxLength", 8);
   }
 });
